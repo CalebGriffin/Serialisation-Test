4a4d2fd04335d54e1eef77b6154a1580e535258e
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2022-10-13 18:52:16 +0100

    Fixed some Leaderboard Issues

--- PacRun/Assets/Scripts/Leaderboard.cs
+++ PacRun/Assets/Scripts/Leaderboard.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using TMPro;
 
 public class Leaderboard : MonoBehaviour
 {
     dreamloLeaderBoard dl;
     public PlayerMovement playerScript;
+    public LevelSetup levelSetup;
     public TMP_InputField inputField;
-    public GameObject leaderboardObj;
+    public GameObject leaderboardObj, textParentObj, nameWarningObj;
+
+    private float sideMargin = 300f, bottomMargin = 150f;
 
     public List<TextMeshProUGUI> textList;
 
@@ -37,13 +41,44 @@ public class Leaderboard : MonoBehaviour
     {
         if (inputField.text == "")
             return;
+        if (inputField.text.Contains("*"))
+        {
+            GrowText();
+            return;
+        }
         this.dl = dreamloLeaderBoard.GetSceneDreamloLeaderboard(playerScript.levelNo);
         Debug.Log(this.dl);
         dl.AddScore(inputField.text, (int)(100000-(playerScript.timerTime * 100)));
+        levelSetup.BackToMenu();
+    }
+
+    public void TextValidation()
+    {
+        if (inputField.text.Contains("*"))
+        {
+            nameWarningObj.SetActive(true);
+        }
+        else
+        {
+            nameWarningObj.SetActive(false);
+            nameWarningObj.GetComponent<TextMeshProUGUI>().margin = new Vector4(300, 0, 300, 150);
+        }
+    }
+
+    private void GrowText()
+    {
+        if (sideMargin >= 10)
+        {
+            sideMargin -= 10;
+            bottomMargin -= 5;
+        }
+        nameWarningObj.GetComponent<TextMeshProUGUI>().margin = new Vector4(sideMargin, 0, sideMargin, bottomMargin);
     }
 
     public void ShowLeaderboard(int levelNo)
     {
+        textParentObj.SetActive(false);
+        leaderboardObj.SetActive(true);
         this.dl = dreamloLeaderBoard.GetSceneDreamloLeaderboard(levelNo);
         dl.GetScores();
         Debug.Log(dl);
@@ -55,31 +90,35 @@ public class Leaderboard : MonoBehaviour
         yield return new WaitUntil(() => dl.highScores != "");
         List<dreamloLeaderBoard.Score> scoreList = dl.ToListHighToLow();
         int count = 0;
+        int num = 1;
         foreach (dreamloLeaderBoard.Score currentScore in scoreList)
         {
-            textList[count].text = $"{count+1}. {currentScore.playerName}";
+            textList[count].text = $"{num}. {UnityWebRequest.UnEscapeURL(currentScore.playerName)}";
             Debug.Log(currentScore.playerName + ": " + currentScore.score.ToString());
             float time = (100000f - currentScore.score) / 100f;
             Debug.Log(time);
-            textList[count+1].text = $"{time:N2}";
+            textList[count+1].text = $"{time:N2}s";
 
+            num++;
             count += 2;
             if (count >= textList.Count)
                 break;
         }
 
-        leaderboardObj.SetActive(true);
+        textParentObj.SetActive(true);
     }
 
     public void HideLeaderboard()
     {
         int count = 0;
+        int num = 1;
         leaderboardObj.SetActive(false);
         foreach (TextMeshProUGUI text in textList)
         {
-            textList[count].text = $"{count+1}. Blank Name";
-            textList[count+1].text = "00.00";
+            textList[count].text = $"{num}. Blank Name";
+            textList[count+1].text = "00.00s";
 
+            num++;
             count += 2;
             if (count >= textList.Count)
                 break;
